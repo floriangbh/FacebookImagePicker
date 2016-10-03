b184c58641d39537d76010833d4e9a1b8928226a
Florian Gabach <florian.gabach@gmail.com>
2016-10-03 09:21:04 +0200

    Load picked picture in Exemple

--- Example/GBHFacebookImagePicker/ViewController.swift
+++ Example/GBHFacebookImagePicker/ViewController.swift
@@ -11,6 +11,7 @@ import GBHFacebookImagePicker
 
 class ViewController: UIViewController, GBHFacebookImagePickerDelegate {
     
+    @IBOutlet weak var pickerImageView: UIImageView!
     @IBOutlet weak var showAlbumButton: UIButton!
 
     override func viewDidLoad() {
@@ -21,6 +22,8 @@ class ViewController: UIViewController, GBHFacebookImagePickerDelegate {
         self.showAlbumButton.addTarget(self,
                                        action: #selector(self.showAlbumClick),
                                        for: UIControlEvents.touchUpInside)
+        
+        self.pickerImageView.contentMode = .scaleToFill
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,12 +41,35 @@ class ViewController: UIViewController, GBHFacebookImagePickerDelegate {
         self.present(navi, animated: true)
     }
     
+    
+    /**
+    * Load the picked picture
+    **/
+    func loadPickedPicture(url: String) {
+        if let url = URL(string: url) {
+            URLSession.shared.dataTask(with: url as URL) { data, response, error in
+                guard let data = data , error == nil else {
+                    print("\nerror on download \(error)")
+                    return
+                }
+                if let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode != 200 {
+                    print("statusCode != 200; \(httpResponse.statusCode)")
+                    return
+                }
+                DispatchQueue.main.async {
+                    self.pickerImageView.image = UIImage(data: data)
+                }
+                }.resume()
+        }
+    }
+    
     // MARK: - GBHFacebookImagePicker Protocol
     
     func facebookImagePicker(imagePicker: UIViewController, didSelectImageWithUrl url: String) {
         imagePicker.dismiss(animated: true, completion: nil)
         
         print("Image URL : \(url)")
+        self.loadPickedPicture(url: url) // Load pic 
     }
     
     func facebookImagePicker(imagePicker: UIViewController, didFailWithError error: Error?) {
