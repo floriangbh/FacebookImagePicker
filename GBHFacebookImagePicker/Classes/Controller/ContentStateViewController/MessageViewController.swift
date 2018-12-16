3411002930862b0838c971479351300b9e1be529
Florian Gabach <florian.gabach@gmail.com>
2018-12-16 17:55:24 +0100

    Code cleanup

---
+++ GBHFacebookImagePicker/Classes/Controller/ContentStateViewController/MessageViewController.swift
@@ -0,0 +1,41 @@
+//
+//  MessageViewController.swift
+//  GBHFacebookImagePicker
+//
+//  Created by Florian Gabach on 17/11/2018.
+//
+
+import UIKit
+
+final class MessageViewController: UIViewController {
+    
+    // MARK: - Var
+    
+    fileprivate var message: String
+    
+    fileprivate lazy var infoLabel: UILabel = {
+        let infoLabel = UILabel()
+        infoLabel.text = message
+        return infoLabel
+    }()
+    
+    // MARK: - Lifecycle
+    
+    init(message: String) {
+        self.message = message
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = FacebookImagePicker.pickerConfig.uiConfig.backgroundColor
+        
+        self.view.addSubview(self.infoLabel)
+        self.infoLabel.pinCenter(to: self.view)
+    }
+}
